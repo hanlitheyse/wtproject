c6273434f66b0fa5be9cb8e7019dc816a1c52424
hanlitheyse <110611829+hanlitheyse@users.noreply.github.com>
2022-09-20 23:27:53 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="mobile-web-app-capable" content="yes"/>
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon"/>
+    <link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon"/>
+    <link rel="apple-touch-icon" href="favicon.ico"/>
+    <link rel="apple-touch-icon-precomposed" href="favicon.ico"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+     integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" 
+     crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <link href="http://fonts.cdnfonts.com/css/ts-kirt" rel="stylesheet">
+     <link href="http://fonts.cdnfonts.com/css/sorren-medium" rel="stylesheet">
+     <link href="http://fonts.cdnfonts.com/css/sweet-milk" rel="stylesheet">      
+                
+                
+                
+    <link rel="stylesheet" href="MIUStyleSheet.css"/>
+    <title>Mix It Up</title>
+    <script></script>
+    <style>
+        body{
+        font-family: 'TS Kirt', sans-serif;
+        background: url("Cocktail_Splash1.jpg") no-repeat center center/cover;
+        width: 100vw;
+        height: 100vh;
+        overflow: hidden;
+        }
+
+    </style>
+</head>
+<body>
+ 
+<?php
+include 'nav.php'
+?>
+
+    <section>
+        <div class="container">
+            <h3>Cocktail Catalogue</h3>
+            <p> A one-stop-shop to discover and create your favourite cocktails. Create the classics from home with our recipe book or find the perfect spot to enjoy something new with our bar finder...  </p>
+            <br>
+            <a href="base.php">discover more</a>
+        </div>
+    </section>
+
+
+</body>
+</html>
